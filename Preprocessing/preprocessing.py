132c91e7d09769ba3c4701dbebda224fe6600efc
Jordi Granja <117774177+jordigb4@users.noreply.github.com>
2024-02-26 22:01:37 +0100

    preprocessing_stable_0

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -1,12 +1,12 @@
-from string import digits
 import re
 
+"""
 spa_path = 'corpora\spa_trn.txt'
 
 with open(spa_path, 'r', encoding='utf-8') as file:
     
     spa_trn = file.read()
-
+"""
 def remove_digits(text):
     #Return: the string of text without digits
 
@@ -30,4 +30,11 @@ def concatenate_whitespaces(text):
     res = re.sub(r'\n','  ', re.sub(r'\t','',text))
     return res
 
-print(repr(concatenate_whitespaces(reduce_whitespaces(lower(remove_digits(spa_trn[:5000]))))))
+def preprocessing(text):
+    return concatenate_whitespaces(reduce_whitespaces(lower(remove_digits(text))))
+
+
+"""
+raw_text = preprocessing(spa_trn)
+print(raw_text[:5000])
+"""
