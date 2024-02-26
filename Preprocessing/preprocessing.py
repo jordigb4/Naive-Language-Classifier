2b7fa9bd626099adb5675f15f3e25ff86244a70a
albertojc6 <133217246+albertojc6@users.noreply.github.com>
2024-02-26 19:39:21 +0100

    preprocessing

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -8,11 +8,26 @@ with open(spa_path, 'r', encoding='utf-8') as file:
     spa_trn = file.read()
 
 def remove_digits(text):
+    #Return: the string of text without digits
 
     res = re.sub(r'\d+','',text)
     return res
 
+def lower(str):
+    #Return: the lowercase string from str
+
+    return str.lower()
+
+def reduce_whitespaces(str):
+    #Return: string with only one whitespace between characters
+
+    res = re.sub(r'\s+', ' ', str)
+    return res
+
 def concatenate_whitespaces(text):
-    res = re.sub(r'\n','  ',re.sub(r'\t','',text))
-    return res 
-print(repr(concatenate_whitespaces(remove_digits(spa_trn[:5000]))))
+    #Return: string with all the phrases concatenated with a double space in the middle
+
+    res = re.sub(r'\n','  ', re.sub(r'\t','',text))
+    return res
+
+print(repr(concatenate_whitespaces(reduce_whitespaces(lower(remove_digits(spa_trn[:5000]))))))
