19f221304fad66d1cac247119590a8482e78f5eb
Jordi Granja Bayot <=>
2024-02-27 15:38:58 +0100

    typing included

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -7,27 +7,26 @@ with open(spa_path, 'r', encoding='utf-8') as file:
     
     spa_trn = file.read()
 """
-def remove_digits(text):
+def remove_digits(text: str) -> str:
     #Return: the string of text without digits
 
     return re.sub(r'\d+','',text)
 
-def lower(text):
+def lower(text: str) -> str: 
     #Return: the lowercase string from str
 
     return str.lower()
 
-def reduce_whitespaces(text):
+def reduce_whitespaces(text: str) -> str:
     #Return: string with only one whitespace between characters
-
     return re.sub(r'\s+', ' ', str)
 
-def concatenate_whitespaces(text):
+def concatenate_whitespaces(text: str) -> str:
     #Return: string with all the phrases concatenated with a double space in the middle
 
     return re.sub(r'\n','  ', re.sub(r'\t','',text))
 
-def preprocessing(text):
+def preprocessing(text:str) -> str:
     return concatenate_whitespaces(reduce_whitespaces(lower(remove_digits(text))))
 
 
