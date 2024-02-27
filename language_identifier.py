b4d468ce3ebb2f7128967681c203b40c648237a7
albertojc6 <133217246+albertojc6@users.noreply.github.com>
2024-02-27 17:59:25 +0100

    orelleta

--- language_identifier.py
+++ language_identifier.py
@@ -1,11 +1,11 @@
 from Preprocessing import preprocessing
 from nltk.collocations import TrigramCollocationFinder
 import numpy as np
+from math import log, exp
 
 class LanguageIdentifier:
 
-    def __init__(self,smoothing = "Lidstone", alpha = 0.5):
-
+    def __init__(self, smoothing = "Lidstone", alpha = 0.5):
      
         self.smoothing = smoothing
         if smoothing == "Lidstone":
@@ -17,22 +17,35 @@ class LanguageIdentifier:
         self.__train()
 
     def __train(self):
+        #Post: creates two attributes that keep training data features, i.e. the number of
+        #      total trigrams in each language and, for each language the count of each trigram.
         
-        preprocessed_corpora = {language : self.__read_corpora(f"corpora/{language}_trn.txt") for language in self.languages}
-
-        self.trigrams_corpora = {language : self.__get_trigrams(preprocessed_corpora[language]) for language in self.languages}
+        self.total_trigrams_corpora = {} #total trigrams counts for each language
+        self.trigrams_corpora = {} #trigrams counts for a language
+        for language in self.languages:
+            corpora = self.__read_corpora(f"corpora/{language}_trn.txt")
+            self.trigrams_corpora[language], self.total_trigrams_corpora[language] = self.__get_trigrams(corpora)
     
-    def __read_corpora(self,path):
+    def __read_corpora(self, path):
+        #Return: preprocessed string from file in path
 
         with open(path, 'r', encoding='utf-8') as file:
             return preprocessing(file.read())
             
-    def __get_trigrams(self,corpora):
+    def __get_trigrams(self, corpora):
+        #Pre: corpora is a string
+        #Return: trigrams freq's from corpora and total count
 
         finder = TrigramCollocationFinder.from_words(corpora)
-        return [tr for tr in finder.ngram_fd.items()]
+        tr_c = {}
+        count = 0
+        for tr, c in finder.ngram_fd.items():
+            tr_c[tr] = c
+            count += c
+        return tr_c, count
+        
     
-    def identify_language(self,path):
+    def identify_language(self, path):
         predicted = np.array()
 
         preprocessed_test = self.__read_corpora(path)
@@ -42,3 +55,26 @@ class LanguageIdentifier:
 
             finder = TrigramCollocationFinder.from_words(phrase)
             trigrams = [tr for tr in finder.ngram_fd.items()]
+
+    def likelihood(self, d, language):
+        #Pre: d is a sequence of character tri-grams
+        #Return: probability of document associated to d writen in 'language'
+
+        sum_logprobs = sum([log(self.LID_n_gram_likelihood(e_j, language)) for e_j in d])
+        return exp(sum_logprobs)
+
+    def LID_n_gram_likelihood(self, e_j, language):
+        #Pre: e_j is a tri-gram character; language is recognizable by the model
+        #Return: MLE with LID smoothing of e_j belong to 'language' based on training corpora
+
+        count_e_j = self.get_count(e_j, language)
+        total_count = self.get_total(language)
+        return (count_e_j + 0.5) / (total_count + 0.5*pow(24, 3))
+
+    def get_count(self, e_j, language):
+        #Pre: e_j is a tri-gram character; language is recognizable by the model
+        #Return: count of e_j in training corpora related to language
+        corpora = self.trigrams_corpora[language]
+        return corpora[e_j]
+
+    
